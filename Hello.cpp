e9eb8cde5113572b4403891f575a53b9ad121e9b
AdrianR0driguez <007923676@coyote.csusb.edu>
2024-02-05 23:12:05 -0800

    added file Hello

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+/***
+   * {This lab seemed to be setting up our compilers and IDE}
+***/
+
+
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
